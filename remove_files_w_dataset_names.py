713e41c9a759b092cdb63f73b648572654103736
annaship <tetya-loshad@yandex.ru>
2013-05-14 15:21:10 -0400

    mysql python rename and remove files; args, works

--- remove_files_w_dataset_names.py
+++ remove_files_w_dataset_names.py
@@ -1,8 +1,18 @@
 import file_names_w_dataset
+import sys
 import shared #use shared to call connection from outside of the module
 
-if __name__ == '__main__':
-    
-  shared.my_conn = file_names_w_dataset.MyConnection('newbpcdb2', 'env454')
-  file_names_w_dataset.File_Names_fromDB().remove_all_new_files()
+if (len(sys.argv) < 3 or sys.argv[1] == "help" or sys.argv[1] == "-h"):
+    print """Please provide rundate and line.
+Ex.: python %s 20130419 1
+This script will create file duplicates with project_dataset file names.
+    """ % sys.argv[0]
+else:
+    rundate = sys.argv[1]
+    lane    = sys.argv[2]
+# path    = sys.argv[3]
 
+    if __name__ == '__main__':
+    
+      shared.my_conn = file_names_w_dataset.MyConnection('newbpcdb2', 'env454')
+      file_names_w_dataset.File_Names_fromDB(rundate, lane).remove_all_new_files()
--- rename_files_to_datasets.py
+++ rename_files_to_datasets.py
@@ -2,13 +2,18 @@ import file_names_w_dataset
 import sys
 import shared #use shared to call connection from outside of the module
 
-print 'sys.argv = '
-print sys.argv
-rundate = sys.argv[1]
-lane    = sys.argv[2]
+if (len(sys.argv) < 3 or sys.argv[1] == "help" or sys.argv[1] == "-h"):
+    print """Please provide rundate and line.
+    Ex.: python %s 20130419 1
+This script will create file duplicates with project_dataset file names.
+    """
+    
+else:
+    rundate = sys.argv[1]
+    lane    = sys.argv[2]
 # path    = sys.argv[3]
 
-if __name__ == '__main__':
-  shared.my_conn = file_names_w_dataset.MyConnection('newbpcdb2', 'env454')
-  file_names_w_dataset.File_Names_fromDB(rundate, lane).rename_files_to_pr_dataset()
+    if __name__ == '__main__':
+      shared.my_conn = file_names_w_dataset.MyConnection('newbpcdb2', 'env454')
+      file_names_w_dataset.File_Names_fromDB(rundate, lane).rename_files_to_pr_dataset()
 
