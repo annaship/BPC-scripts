446e078a5edd47b9c4c2a5cd9b5e06cd446fcc23
annaship <tetya-loshad@yandex.ru>
2013-07-02 17:03:19 -0400

    no fast-merge

--- run_partial_overlap_clust.sh
+++ run_partial_overlap_clust.sh
@@ -7,6 +7,7 @@ do
   file_idx_key=$(basename "$fullfile")
   file_idx_key="${file_idx_key%.*}"
   #echo "file_idx_key = $file_idx_key"
-  clusterize /bioware/merens-illumina-utils/scripts/merge-illumina-pairs --fast-merge --compute-qual-dicts $fullfile $file_idx_key
+  # clusterize /bioware/merens-illumina-utils/scripts/merge-illumina-pairs --fast-merge --compute-qual-dicts $fullfile $file_idx_key
+  clusterize /bioware/merens-illumina-utils/scripts/merge-illumina-pairs $fullfile $file_idx_key
 done
 
