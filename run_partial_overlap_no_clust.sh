9db748dde7fd681c85c9f8944f5544990e709851
annaship <tetya-loshad@yandex.ru>
2013-07-24 17:44:16 -0400

    run_partial_overlap_no_clust.sh add 30check

---
+++ run_partial_overlap_no_clust.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for fullfile in *.ini
+do
+  echo "============="
+  echo $fullfile
+  file_idx_key=$(basename "$fullfile")
+  file_idx_key="${file_idx_key%.*}"
+  #echo "file_idx_key = $file_idx_key"
+  /bioware/merens-illumina-utils/scripts/merge-illumina-pairs --enforce-Q30-check $fullfile
+done
+
