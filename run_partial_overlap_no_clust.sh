b9a1ebc161404cf57bf17496e9798275c6e39a72
annaship <tetya-loshad@yandex.ru>
2013-07-24 17:46:30 -0400

    run_partial_overlap_no_clust.sh add slow-merge

--- run_partial_overlap_no_clust.sh
+++ run_partial_overlap_no_clust.sh
@@ -7,6 +7,6 @@ do
   file_idx_key=$(basename "$fullfile")
   file_idx_key="${file_idx_key%.*}"
   #echo "file_idx_key = $file_idx_key"
-  /bioware/merens-illumina-utils/scripts/merge-illumina-pairs --enforce-Q30-check $fullfile
+  /bioware/merens-illumina-utils/scripts/merge-illumina-pairs --enforce-Q30-check --slow-merge $fullfile
 done
 
