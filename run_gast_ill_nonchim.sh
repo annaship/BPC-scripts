304d50f88f011a93b2b66de220b5a9c403ecd506
annaship <ashipunova@as-macbook.home>
2014-05-22 10:17:27 -0400

    correct file names for its

--- run_gast_ill_nonchim.sh
+++ run_gast_ill_nonchim.sh
@@ -6,20 +6,21 @@ options=("*.unique.nonchimeric.fa" "*.unique.nonchimeric.fa for Fungi (ITS1)" "*
 
 echo "$title"
 PS3="$prompt "
+ITS_OPTION=""
 select opt in "${options[@]}"; do 
 
     case "$REPLY" in
 
     "*.unique.nonchimeric.fa" )   NAME_PAT=$REPLY; UDB_NAME=refv4v5; echo "You picked option $REPLY"; break;;
-    "*.unique.nonchimeric.fa for Fungi (ITS1)" )   NAME_PAT=$REPLY; UDB_NAME=refits1; echo "You picked option $REPLY"; break;;
+    "*.unique.nonchimeric.fa for Fungi (ITS1)" )   NAME_PAT="*.unique.nonchimeric.fa"; UDB_NAME=refits1; ITS_OPTION=" -full "; echo "You picked option $REPLY"; break;;
     "*-PERFECT_reads.fa.unique" ) NAME_PAT=$REPLY; UDB_NAME=refv6;   echo "You picked option $REPLY"; break;;
     "*-PERFECT_reads.fa.unique for Archaeae" ) NAME_PAT="*-PERFECT_reads.fa.unique"; UDB_NAME=refv6a;   echo "You picked option $REPLY"; break;;
     "*MAX-MISMATCH-3.unique" ) NAME_PAT=$REPLY; UDB_NAME=refv6long;   echo "You picked option $REPLY"; break;;
 
     1 ) NAME_PAT=${options[0]};                    UDB_NAME=refv4v5; echo "You picked option $REPLY"; break;;
-    2 ) NAME_PAT=${options[1]};                    UDB_NAME=refits1; echo "You picked option $REPLY"; break;;
+    2 ) NAME_PAT="*.unique.nonchimeric.fa";        UDB_NAME=refits1; ITS_OPTION=" -full "; echo "You picked option $REPLY"; break;;
     3 ) NAME_PAT=${options[2]};                    UDB_NAME=refv6; echo "You picked option $REPLY"; break;;
-    4 ) NAME_PAT=${options[3]};                    UDB_NAME=refv6a; echo "You picked option $REPLY"; break;;
+    4 ) NAME_PAT="*-PERFECT_reads.fa.unique";      UDB_NAME=refv6a; echo "You picked option $REPLY"; break;;
     5 ) NAME_PAT=${options[4]};                    UDB_NAME=refv6long; echo "You picked option $REPLY"; break;;
 
     # $(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
@@ -30,9 +31,11 @@ select opt in "${options[@]}"; do
 done
 
 echo "UDB_NAME = $UDB_NAME.udb"
+echo "ITS_OPTION = $ITS_OPTION"
+
 for file in $NAME_PAT
 do
   echo "============="
   echo $file
-  clusterize /bioware/seqinfo/bin/gast_ill -saveuc -nodup  -in $file -db /xraid2-2/g454/blastdbs/gast_distributions/$UDB_NAME.udb -rtax /xraid2-2/g454/blastdbs/gast_distributions/$UDB_NAME.tax -out $file.gast
+  clusterize /bioware/seqinfo/bin/gast_ill -saveuc -nodup $ITS_OPTION -in $file -db /xraid2-2/g454/blastdbs/gast_distributions/$UDB_NAME.udb -rtax /xraid2-2/g454/blastdbs/gast_distributions/$UDB_NAME.tax -out $file.gast
 done
