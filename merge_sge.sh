caf0a68a170d4e9ef17a20046db58d707349b799
annaship <tetya-loshad@yandex.ru>
2014-06-12 15:45:55 -0400

    interm

--- merge_sge.sh
+++ merge_sge.sh
@@ -1,3 +1,49 @@
+#!/bin/bash
+ini_count=`ls *.ini | wc -l`
+echo $ini_count
+ini_list=`ls *.ini`
+echo $ini_listi
+
+#$ -cwd
+#$ -S /bin/bash
+# This next line is the important one, it says loop 1-n times; Rich's
+link shows some variations
+#$ -t 1-59609
+
+INFILE=`sed -n "${SGE_TASK_ID}p" $ini_list`
+
+username=`whoami`
+echo "$username@mbl.edu"
+
+make_sge_script()
+{
+    cat << InputComesFromHERE > $job_file_name_prefix.sge_script.sh
+#!/bin/bash
+
+#$ -cwd
+#$ -S /bin/bash
+#$ -N $file_name
+# Giving the name of the output log file
+#$ -o $file_name.sge_script.sh.log
+# Combining output/error messages into one file
+#$ -j y
+# Send mail to these users
+#$ -M $username@mbl.edu
+# Send mail at job end; -m eas sends on end, abort, suspend.
+#$ -m eas
+#$ -t 1-$ini_count
+# Now the script will iterate $ini_count times.
+
+echo ".$job_file_name_prefix.\$SGE_TASK_ID.job.sh"
+./$job_file_name_prefix.\$SGE_TASK_ID.job.sh
+InputComesFromHERE
+
+chmod u+x $job_file_name_prefix.sge_script.sh
+}
+
+
+
+===========
 20130408_1_b_reads_overlap
 create_chimera_cmd - clusterize in pipline
 #!/bin/bash
@@ -80,3 +126,15 @@ blastx -query $INFILE -db refseq_protein -outfmt 5 -max_target_seqs 1
 echo ".sequence_ill.txt.out_db.$SGE_TASK_ID.job.sh"
 ./sequence_ill.txt.out_db.$SGE_TASK_ID.job.sh
 sequence_ill.txt.out_db.sge_script.sh (END) 
+===
+#!/bin/sh
+ 
+start=$SGE_TASK_ID
+let end="$start + 1"
+SEQNAME=`sed -n "${start}p" $1`
+SEQUENCE=`sed -n "${end}p" $1`
+
+echo -e "$SEQNAME\n$SEQUENCE" | blastx -db refseq_protein -outfmt 5 -max_target_seqs 1 -out $SGE_TASK_ID.br
+
+2) command line:
+amount=$(echo `wc -l FILENAME.fa` | echo "`cut -f1 -d' '` +1" |bc); qsub -cwd -t 1-$amount:2 ./SCRIPTNAME.sh FILENAME.fa
