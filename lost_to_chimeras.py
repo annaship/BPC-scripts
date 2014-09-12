df737241e9b7ffb4931e5f5642870a09f1fb7217
annaship <tetya-loshad@yandex.ru>
2014-09-12 18:01:34 -0400

    lost_to_chimeras: cleaning up

--- lost_to_chimeras.py
+++ lost_to_chimeras.py
@@ -6,10 +6,6 @@ from time import time
 import subprocess
 import difflib
 
-base_suffix = "MERGED-MAX-MISMATCH-3"
-unique_suffix = ".unique"
-nochimeric_suffix = ".unique.nonchimeric.fa"
-
 def get_file_names(current_dir):
     files = []
     for (dirpath, dirname, filenames) in os.walk(current_dir):
@@ -31,22 +27,22 @@ def get_basenames(filenames):
 def wccount(filename):
     return subprocess.check_output(['wc', '-l', filename]).split()[0]
 
-def get_files_content(file_basename):
+def get_head_lines(file_basename):
     file_open = open(file_basename, 'r')
     return  [l for l in file_open.readlines() if l.startswith('>')]
-  
 
 def get_diff(file1_content, file2_content):
-    only_in_chimeric = []
+    only_not_chimeric = []
     
     diff = difflib.ndiff(file1_content, file2_content)
-    only_in_chimeric.append(x[2:] for x in diff if (x.startswith('- ')))
+    """ '- ' 	line unique to file1_content """
+    only_not_chimeric.append(x[2:] for x in diff if (x.startswith('- ')))
 
-    return only_in_chimeric
+    return only_not_chimeric
 
-def get_size(only_in_chimeric):
+def get_size(only_not_chimeric):
     size_chim = 0
-    for diff_next in only_in_chimeric:  
+    for diff_next in only_not_chimeric:  
       try:
           while 1:
               size_chim += int(diff_next.next().split(':')[-1])
@@ -63,19 +59,18 @@ def get_percent(before_chim, chim_size):
 current_dir    = os.getcwd()
 print current_dir
 
-before_file_suffix = "_MERGED-MAX-MISMATCH-3.unique"
-after_file_suffix = "_MERGED-MAX-MISMATCH-3.unique.nonchimeric.fa"
+base_suffix = "MERGED-MAX-MISMATCH-3"
+unique_suffix = ".unique"
+nochimeric_suffix = ".unique.nonchimeric.fa"
 
 filenames = get_basenames(get_file_names(current_dir))
 
 for file_basename in filenames:
-    before_reads = 0
-    after_reads = 0
     unique_file_name = file_basename + unique_suffix
     nochimeric_file_name = file_basename + nochimeric_suffix
 
-    only_in_chimeric = get_diff(get_files_content(unique_file_name), get_files_content(nochimeric_file_name))
-    chim_size = get_size(only_in_chimeric)
+    only_not_chimeric = get_diff(get_head_lines(unique_file_name), get_head_lines(nochimeric_file_name))
+    chim_size = get_size(only_not_chimeric)
     before_chim =  wccount(file_basename)
     perc = get_percent(before_chim, chim_size)
     
@@ -84,8 +79,3 @@ for file_basename in filenames:
     print "Total amount of not unique seq: %s" % before_chim
     print "Lost to chimeras: %s" % chim_size
     print "Lost to chimeras (percentage): %s" % round(perc, 2)
-    
-    
-# todo:
-# benchmark count all in not uniq and sum all of sizes notchimeric and get the difference
-
