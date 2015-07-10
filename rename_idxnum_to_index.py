b4cd98a75300054e6ce2a95d17024c735e364106
annaship <tetya-loshad@yandex.ru>
2015-07-10 16:15:11 -0400

    help

--- rename_idxnum_to_index.py
+++ rename_idxnum_to_index.py
@@ -6,7 +6,7 @@ import shared #use shared to call connection from outside of the module
 if (len(sys.argv) < 3 or sys.argv[1] == "help" or sys.argv[1] == "-h"):
     print """Please provide domain and dna_region.
 Ex.: %s bacteria v6 
-If your raw file names start with 'IDX' instead of an actual index run %s to rename the files. Run it in the directory with the raw files on any server.
+If your raw file names start with "IDX" (like "IDX8_S8_L001_R2_001") instead of an actual index run %s to rename the files. Run it in the directory with the raw files on any server.
     """ % (sys.argv[0], sys.argv[0])
 else:
     domain    = sys.argv[1]
