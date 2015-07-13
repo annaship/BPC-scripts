ebedc906a56e0d0a20b70c3b06629b3ec5170d60
annaship <tetya-loshad@yandex.ru>
2015-07-13 10:15:23 -0400

    help

--- rename_idxnum_to_index.py
+++ rename_idxnum_to_index.py
@@ -10,7 +10,9 @@ if (len(sys.argv) < 3 or sys.argv[1] == "help" or sys.argv[1] == "-h"):
     dna_regions    = rename_idxnum_to_index_class.Index_Numbers_fromDB("","").get_dna_region_from_db()   
     
     print """
-    If your raw file names start with "IDX" (like "IDX8_S8_L001_R2_001") instead of an actual index run %s to rename the files. Run it in the directory with the raw files on any server.
+    If your raw file names start with "IDX" (like "IDX8_S8_L001_R2_001") instead of an actual index run 
+python %s 
+    to rename the files. Run it in the directory with the raw files on any server.
     
     Please provide domain and dna_region.
     Ex.: python %s bacteria v6 
