50e039173306f63d39bcede915442fe94f9318f8
annaship <tetya-loshad@yandex.ru>
2015-11-09 17:43:33 -0500

    verbose

--- get_hvr_cut.py
+++ get_hvr_cut.py
@@ -8,7 +8,7 @@ def usage():
 def main(argv):
     inputfile  = ''
     outputfile = 'out_region.tsv'
-    verbous    = ''
+    verbose    = False
     
     try:
       opts, args = getopt.getopt(argv, "hvi:o:", ["ifile=","ofile="])
@@ -19,15 +19,15 @@ def main(argv):
     for opt, arg in opts:
       if opt == "-v":
         verbose = True
-      if opt == '-h':
+      elif opt == '-h':
         usage()
         sys.exit()
       elif opt in ("-i", "--ifile"):
         inputfile = arg
       elif opt in ("-o", "--ofile"):
         outputfile = arg
-        
-      return (inputfile, outputfile, verbous)
+              
+    return (inputfile, outputfile, verbose)
 
 
 def read_file(inputfile):
@@ -38,48 +38,50 @@ def write_file(outputfile):
   f = open(outputfile, 'w')
   return f
 
+def print_stats(sequence, v9_cut):
+  print "Full length sequence:  %s" % len(sequence)
+  print "Forward primer starts: %s" % re.search('TTGTACACACCGCCC', sequence).start()
+  print "Reverse primer starts: %s" % re.search('GTAGGTGAACCTGC.GAAGG', sequence).start()
+  print "v9 length:             %s" % len(v9_cut)
 
 def get_region(sequence):
   hvrsequence_119_1 = re.sub('^.+TTGTACACACCGCCC', '', sequence)
   
-  print "Full length sequence: %s" % len(sequence)
-  print "Forward primer starts: %s" % re.search('TTGTACACACCGCCC', sequence).start()
-  print "Reverse primer starts: %s" % re.search('GTAGGTGAACCTGC.GAAGG', sequence).start()
   v9_cut = re.sub('GTAGGTGAACCTGC.GAAGG.+', '', hvrsequence_119_1)
-  print "v9 length: %s" % len(v9_cut)
-  
+    
   return v9_cut
 
-def process(line):
-    print line
-    # refssu_name_id    = line.split("\t")[0]
-    # clean_taxonomy_id = line.split("\t")[1]
-    # sequence          = line.split("\t")[2]
-    # 
-    # v9_cut = get_region(sequence)
-    # print "v9_cut = %s" % v9_cut
-    # 
-    # 
+def process(line, verbose):
+    # print "process line"
+    # print line
+    refssu_name_id    = line.split("\t")[0]
+    clean_taxonomy_id = line.split("\t")[1]
+    sequence          = line.split("\t")[2]
+         
+    v9_cut = get_region(sequence)
+    print "v9_cut = %s" % v9_cut
+
+    if (verbose):
+      print_stats(sequence, v9_cut)
+    
     # print "refssu_name_id = %s, clean_taxonomy_id = %s, hvrsequence_119 = %s\n===" % (refssu_name_id, clean_taxonomy_id, sequence)
 
     
 # 467540	2	ACCTGAGTGCTATTGGGTTTGCTAAAGACATGCAAGTGGAATGTCTCTTCGGAGGCATCGCGAAAGGCTCAGTAACACGTCGCCAATCTGCCCTGTGGACGGGAATAACCTCGGGAAACTGAGACTAATCCCCGATAAGTATGGACTCCTGGAAAGGGCCAATATTTAATGGTCTTCGGATCGCCACAGGATGAGGCCGCGGCCGATTAGCTAGTAAGTGATGTAACGGATCACTTAGGCATTGATCGGTAGGGGCTATGAGAGTAGGAGCCCCGAGAAGGACACTTAGACACTGGTCCTAGCACTACGGTGTGCAGCAGTCGGGAATCGTGCCCAATGCGCGAAAGCGTGAGGCCGCGAACCCAAGTGCTAGGGTTACCCCCTAGCTGTGATGGAGTGTTTAAAGCTCTAACAGCAAGCAGAGGGCAAGGGTGGTGCCAGCCGCCGCGGTAAAACCAGCTCTGCGAGTGCTCAGGACGATTATTGGGCTTAAAGCATCCGTAGCCGGGTAAGTAGGTCCCTGATCAAATCTGCAAGCTTAACTTGTAGGCTGTCAAGGATACCACTAACCTAGGGAATAGGAGAGGTGAACGGTACTGCGAGAGAAGCGGTGAAATGCGTTGATTCTCGCAGGACCCACAGTGGCGAAGGCGGTTCACTGGAATATCTCCGACGGTGATGGATGAAAGCCAGGGGAGCGAAAGGGATTAGAGACCCCTGTAGTCCTGGCCGTAAACGATGAGGATTAGGTGTTGGTTATGGCTAAAGGGCCTGATCAGTGCCAAAGGGAAACTATTAAATCCTCCGCCTGGGGAGTACGGTCGCAAGGCTGAAACTTAAATGAATTGACGGGAAAGCGCCACAAGGCACGGGATGTGTGGTTTAATTCGACTCAACGCGAGGAAACTCACCTGGGGCGACTGTTAAATGTGAGTCAGGCTGAAGACCTTACTCGAATAAAACAGAGAGGTAGTGCATGGCCGTCTCAAGCTCGTGCCGTGAGGTGTGCTCTTAAGTGAGTAAACGAGCGAGACCCGCGTCCCTATTTGCTAAGAGCAAGCTTCGGCTTGGCTGAGGACAATAGGGAGATCGCTATCGATGAAGATAGATGAAAGGGCGGGCCACGGCAGGTCAGTATGCTCCTAATCCCCAGGGCCACACACGCATCACAATGAGTAGGACAATGAGAGGCGACCCCGAAAGGGGAAGCGGACCCCCAAACCTGCTCGCAGTAGGGATCGAGGTCTGTAACCGACCTCGTGAACATGGAGCGCCTAGTATCCGTGTGTCATCATCGCACGGAGAATACGTCCCCGCTTTTTGTACACACCGCCCGTCGTTGCAACGAAGTGAGGTTCGGTTGAGGTTGGGCTGTTACAGCTTATTCGAAATTGGGCTTCGCGACGATGCAA
 
 if __name__ == "__main__":
-    (inputfile, outputfile, verbous) = main(sys.argv[1:])
+    (inputfile, outputfile, verbose) = main(sys.argv[1:])
     print 'Input file is "%s"' % inputfile
     print 'Output file is "%s"' % outputfile
 
     inputfile_content = read_file(inputfile)
-    print "inputfile_conternt = %s" % inputfile_content
+    # print "inputfile_conternt = %s" % inputfile_content
 
     open_outputfile = write_file(outputfile)   
 
 
     for line in inputfile_content:
-      print "line"
-      print line
-        # process(line.strip())
+      process(line.strip(), verbose)
 
 #     line      = line.strip()
 #     
