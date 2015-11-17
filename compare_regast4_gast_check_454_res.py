74e8ba998b66938bc9cd7bfdedcb7bd4ee7a3802
annaship <tetya-loshad@yandex.ru>
2015-11-17 13:27:53 -0500

    fix

--- compare_regast4_gast_check_454_res.py
+++ compare_regast4_gast_check_454_res.py
@@ -23,13 +23,13 @@ with open(filename) as in_file:
     elif line.startswith("total_trimseq"):
       total_trimseq = int(next(in_file))
 
-    elif line.startswith("total_gast_200"):      
-      total_gast_200 = int(next(in_file))
-      print "For %s numbers (%s, %s) are equal is %s" % (line.rstrip(), total_trimseq, total_gast_200, compare_res(total_trimseq, total_gast_200))
+    elif line.startswith("total_gast_2"):      
+      total_gast_2 = int(next(in_file))
+      print "For %s numbers (%s, %s) are equal is %s" % (line.rstrip(), total_trimseq, total_gast_2, compare_res(total_trimseq, total_gast_2))
 
     elif line.startswith("total_gast_trimseq"):
       total_gast_trimseq = int(next(in_file))
-      print "For %s numbers (%s, %s) are equal is %s" % (line.rstrip(), total_gast_200, total_gast_trimseq, compare_res(total_gast_200, total_gast_trimseq))
+      print "For %s numbers (%s, %s) are equal is %s" % (line.rstrip(), total_gast_2, total_gast_trimseq, compare_res(total_gast_2, total_gast_trimseq))
             
     elif line.startswith("total_gast_concat_trimseq"):
       total_gast_concat_trimseq = int(next(in_file))
