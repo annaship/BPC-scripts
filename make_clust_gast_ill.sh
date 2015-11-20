1db5d200756189a4bb260855791ed083256124a4
annaship <tetya-loshad@yandex.ru>
2015-11-20 18:59:06 -0500

    put re and uc into gast_silva119

--- make_clust_gast_ill.sh
+++ make_clust_gast_ill.sh
@@ -2,8 +2,11 @@
 
 gunzip *MERGED-MAX-MISMATCH-3.unique.nonchimeric.fa.gz
 ls *MERGED-MAX-MISMATCH-3.unique.nonchimeric.fa >nonchimeric_files.list
+mkdir gast_silva119
+# cd gast_silva119
 FILE_NUMBER=`wc -l < nonchimeric_files.list`
 echo "total files = $FILE_NUMBER"
+# DIRECTORY_NAME="DIRECTORY_NAME"
 DIRECTORY_NAME=`pwd | sed 's,^/xraid2-2/g454/run_new_pipeline/illumina/\(.*\)/\(lane_1_[^/]*\)/.*,\1_\2,g'`
 echo "DIRECTORY_NAME = $DIRECTORY_NAME"
 NAME_PAT="*.unique.nonchimeric.fa"
@@ -30,7 +33,7 @@ cat >clust_gast_ill_$DIRECTORY_NAME.sh <<InputComesFromHERE
   INFILE=\`sed -n "\${SGE_TASK_ID}p" \$LISTFILE\`
   echo "file name is \$INFILE"
 
-  echo "/bioware/seqinfo/bin/gast_ill -saveuc -nodup $ITS_OPTION -in \$INFILE -db /workspace/ashipunova/silva/119/regast/gast_distributions_119/$UDB_NAME.fa -rtax /workspace/ashipunova/silva/119/regast/gast_distributions_119/$UDB_NAME.tax -out ${PWD}/\$INFILE.gast"
+  echo "/bioware/seqinfo/bin/gast_ill -saveuc -nodup $ITS_OPTION -in \$INFILE -db /workspace/ashipunova/silva/119/regast/gast_distributions_119/$UDB_NAME.fa -rtax /workspace/ashipunova/silva/119/regast/gast_distributions_119/$UDB_NAME.tax -out ${PWD}/gast_silva119/\$INFILE.gast -uc ${PWD}/gast_silva119/\$INFILE.uc"
 
 InputComesFromHERE
 
