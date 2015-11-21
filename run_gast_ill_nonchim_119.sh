39506d9f21b0d1d695ffe4aea9f2208d292ccbcc
annaship <tetya-loshad@yandex.ru>
2015-11-20 20:13:20 -0500

    SGE vs. clusterize

--- run_gast_ill_nonchim_119.sh
+++ run_gast_ill_nonchim_119.sh
@@ -37,14 +37,52 @@ done
 echo "UDB_NAME = $UDB_NAME.udb"
 echo "ITS_OPTION = $ITS_OPTION"
 echo "NAME_PAT = $NAME_PAT"
-echo "Files number: = `echo $NAME_PAT | wc -w`"
 
-for file in ../$NAME_PAT
-do
-  file_name=$(basename $file)
-  echo "============="
-  #echo $file
-  echo "clusterize /bioware/seqinfo/bin/gast_ill -saveuc -nodup $ITS_OPTION -in $file -db /workspace/ashipunova/silva/119/regast/gast_distributions_119/$UDB_NAME.fa -rtax /workspace/ashipunova/silva/119/regast/gast_distributions_119/$UDB_NAME.tax -out ${PWD}/$file_name.gast"
-  clusterize /bioware/seqinfo/bin/gast_ill -saveuc -nodup $ITS_OPTION -in $file -db /workspace/ashipunova/silva/119/regast/gast_distributions_119/$UDB_NAME.fa -rtax /workspace/ashipunova/silva/119/regast/gast_distributions_119/$UDB_NAME.tax -out ${PWD}/$file_name.gast
+# gunzip first!
+# gunzip *MERGED-MAX-MISMATCH-3.unique.nonchimeric.fa.gz
+DIRECTORY_NAME=`pwd`
 
-done
+mkdir gast_silva119
+ls *MERGED-MAX-MISMATCH-3.unique.nonchimeric.fa >gast_silva119/nonchimeric_files.list
+
+cd gast_silva119
+
+FILE_NUMBER=`wc -l < nonchimeric_files.list`
+echo "total files = $FILE_NUMBER"
+# RUN_LANE="RUN_LANE"
+RUN_LANE=`pwd | sed 's,^/xraid2-2/g454/run_new_pipeline/illumina/\(.*\)/\(lane_1_[^/]*\)/.*,\1_\2,g'`
+echo "RUN_LANE = $RUN_LANE"
+# NAME_PAT="*.unique.nonchimeric.fa"
+# UDB_NAME="refv4v5"
+# ITS_OPTION=""
+
+
+cat >clust_gast_ill_$RUN_LANE.sh <<InputComesFromHERE
+#!/bin/bash
+#$ -cwd
+#$ -S /bin/bash
+#$ -N clust_gast_ill_$RUN_LANE.sh
+# Giving the name of the output log file
+#$ -o clust_gast_ill_$RUN_LANE.sh.sge_script.sh.log
+# Combining output/error messages into one file
+#$ -j y
+# Send mail to these users
+#$ -M ashipunova@mbl.edu
+# Send mail at job end; -m eas sends on end, abort, suspend.
+#$ -m eas
+#$ -t 1-$FILE_NUMBER
+# Now the script will iterate $FILE_NUMBER times.
+
+  . /xraid/bioware/Modules/etc/profile.modules
+  module load bioware
+
+  LISTFILE=./nonchimeric_files.list
+  INFILE=\`sed -n "\${SGE_TASK_ID}p" \$LISTFILE\`
+  echo "file name is \$INFILE"
+
+  echo "/bioware/seqinfo/bin/gast_ill -saveuc -nodup $ITS_OPTION -in $DIRECTORY_NAME/\$INFILE -db /workspace/ashipunova/silva/119/regast/gast_distributions_119/$UDB_NAME.fa -rtax /workspace/ashipunova/silva/119/regast/gast_distributions_119/$UDB_NAME.tax -out $DIRECTORY_NAME/gast_silva119/\$INFILE.gast -uc $DIRECTORY_NAME/gast_silva119/\$INFILE.uc"
+
+  # /bioware/seqinfo/bin/gast_ill -saveuc -nodup $ITS_OPTION -in $DIRECTORY_NAME/\$INFILE -db /workspace/ashipunova/silva/119/regast/gast_distributions_119/$UDB_NAME.fa -rtax /workspace/ashipunova/silva/119/regast/gast_distributions_119/$UDB_NAME.tax -out $DIRECTORY_NAME/gast_silva119/\$INFILE.gast -uc $DIRECTORY_NAME/gast_silva119/\$INFILE.uc
+
+InputComesFromHERE
+
