2bf1f3a0643fac6955aefd1e63e384888f268fa7
annaship <tetya-loshad@yandex.ru>
2016-03-30 14:13:16 -0400

    demultiplexed qiita fa

--- demultiplex_qiita.py
+++ demultiplex_qiita.py
@@ -35,56 +35,45 @@ def make_file_names(headlines_dict):
 
 def open_out_sample_files(inputfile_content_ids):
     sample_names = make_file_names(inputfile_content_ids)
-    out_files = dict([(sample, fastalib.FastaOutput(sample)) for sample in sample_names])
+    out_files = {}
+    for sample in sample_names:
+      out_files[sample] = open(sample, "a")
+    # out_files = dict([(sample, fastalib.FastaOutput(sample)) for sample in sample_names])
     return out_files
 
-def close_dataset_files():
-    [o_file[1].close() for o_file in self.out_files.items()] 
+def close_sample_files(out_files):
+    [o_file[1].close() for o_file in out_files.items()] 
     return
 
+def write_id(opened_file, id):
+    opened_file.write('>%s\n' % id)
 
-# def make_sammple_dict():
-#   sample_dict = collections.defaultdict(lambda: collections.defaultdict(list))
-#   for head, seq in fa_dict.items():
-#     print head
-#     print seq
-#     sample_name = head.split("_")[0]
-#     print sample_name
-#     sample_dict[sample_name][head] = seq
-#   return sample_dict
-# 
-# def read_file(inputfile):
-#   with open (inputfile, "r") as myfile:
-#     return myfile.readlines()
-# 
-# def open_file_to_write(outputfile):
-#   f = open(outputfile, 'w')
-#   return f
-# 
-# def make_output_line(line, refhvr_cut):
-#     refssu_name_id    = line.split("\t")[0]
-#     clean_taxonomy_id = line.split("\t")[1]
-#     return refssu_name_id + "\t" + clean_taxonomy_id + "\t" + refhvr_cut
-
-def process(line, verbose, f_primer, r_primer):
-    refhvr_cut = ""
-    sequence   = line.split("\t")[2]       
-    refhvr_cut = get_region(sequence, f_primer, r_primer)
-
-    if (verbose):
-      print_stats(sequence, refhvr_cut, f_primer, r_primer)
+def write_seq(opened_file, seq):
+    opened_file.write('%s\n' % seq)
     
-    return make_output_line(line, refhvr_cut)
-    
-    # print "refssu_name_id = %s, clean_taxonomy_id = %s, hvrsequence_119 = %s\n===" % (refssu_name_id, clean_taxonomy_id, sequence)
-
 if __name__ == "__main__":
     (inputfile) = get_args(sys.argv[1:])
     print 'Input file is "%s"' % inputfile
     
     inputfile_content = fastalib.ReadFasta(inputfile)
 
-    open_out_sample_files(inputfile_content.ids)
+    out_files = open_out_sample_files(inputfile_content.ids)
+    # print out_files
+    
+    fa_dictionary = dict(zip(inputfile_content.ids, inputfile_content.sequences))
+    for id, seq in fa_dictionary.items():
+      file_name = id.split("_")[0] + ".fa"
+      # fastalib.FastaOutput(file_name).write_id(id)
+      # fastalib.FastaOutput(file_name).write_seq(seq)
+      write_id(out_files[file_name], id)
+      write_seq(out_files[file_name], seq)
+    
+    close_sample_files(out_files)
+      # print fastalib.FastaOutput()
+    # print dictionary
+    # for out_file in out_files:
+    #   
+    # while inputfile_content.next()
     # print inputfile_content.ids
     # print inputfile_content.sequences
 
