d026bd783397adbd0d3ed00d34aa36bfa556ca23
annaship <tetya-loshad@yandex.ru>
2016-05-12 13:33:16 -0400

    make ill_stat for new hseq too

--- take_ill_stats.py
+++ take_ill_stats.py
@@ -4,6 +4,7 @@ class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
+    OKRED = '\033[31m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
@@ -11,11 +12,12 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 import os
-count_all_reads = 0
+count_all_reads  = 0
 count_good_reads = 0
-counts_all = 0
-count_good = 0
-color = ""
+counts_all       = 0
+count_good       = 0
+color            = ""
+rsum             = 0
 
 files = []
 current_dir = os.getcwd()
@@ -25,7 +27,21 @@ for (dirpath, dirname, filenames) in os.walk(current_dir):
 
 print 'From iu-merge-pairs readme: P value is the ratio of the number of mismatches and the length of the overlap.\nMerged sequences can be discarded based on this ratio. The default is 0.3.'
 print "Number of pairs analyzed; Merged total"
-    
+
+def no_data_msg():
+  color = bcolors.OKRED
+  print "NO DATA"
+  color = bcolors.ENDC
+  
+def sum_reads(count_reads, counts_all):  
+  try:
+    return int(count_reads) + int(counts_all)
+  except ValueError:
+    count_reads = 0
+    no_data_msg()
+  except:
+    raise
+  
 for f in sorted(files):
   if f.endswith("_STATS"):
     file = open(f)
@@ -35,14 +51,28 @@ for f in sorted(files):
         if not line:
             break
         if line.startswith("Number of pairs analyzed"):
-          counts_all = line.split()[4]
+          counts_all = line.split()[-1]
           print counts_all
-          count_all_reads += int(counts_all)
+          count_all_reads = sum_reads(count_all_reads, counts_all)
+          # print "count_all_reads interim = "
+          # print count_all_reads
         if line.startswith("Merged total"):
-          count_good = line.split()[2]
+          count_good = line.split()[-1]
           print count_good
-          count_good_reads += int(count_good)
-    percent = int(count_good) * float(100) / int(counts_all)
+          count_good_reads = sum_reads(count_good_reads, count_good)
+          # print "count_good_reads interim = "
+          # print count_good_reads
+          
+    try:
+      percent = int(count_good) * float(100) / int(counts_all)
+    except ZeroDivisionError:
+      percent = 0
+      no_data_msg()
+    except ValueError:
+      percent = 0
+      no_data_msg()
+    except:
+      raise
     if percent < 80 and percent > 60:
 	color = bcolors.OKBLUE
     elif percent < 60:
@@ -53,7 +83,7 @@ for f in sorted(files):
                 
 print "="*50    
 print current_dir
-print "count_all_reads = %s" % (count_all_reads)
+print "count_all_reads  = %s" % (count_all_reads)
 print "count_good_reads = %s" % (count_good_reads)
 try:
     print "percent kept for the run = %s%%" % (count_good_reads * 100 / count_all_reads)
