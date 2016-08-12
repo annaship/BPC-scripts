e1112cbd5c870f35ce52f84040b76b01ba0c7d75
annaship <tetya-loshad@yandex.ru>
2016-08-12 18:16:04 -0400

    field names in args

--- py_scripts_mix/add_annotation.py
+++ py_scripts_mix/add_annotation.py
@@ -14,7 +14,10 @@ class CsvTools():
 
       self.delimiter = args.delimiter
       self.quotechar = args.quotechar
-
+      
+      self.common_key_from   = args.common_key_from
+      self.common_key_to     = args.common_key_to
+      self.field_to_add_name = args.field_to_add_name
 
     def read_from_file(self):
       from_headers, from_reader = self.get_reader(self.from_file_name)
@@ -22,17 +25,28 @@ class CsvTools():
 
     def read_to_file(self):
       self.to_headers, to_reader = self.get_reader(self.to_file_name)
-      self.to_file_content  = self.parce_csv(to_reader)
+      self.to_file_content       = self.parce_csv(to_reader)
 
     def make_res_dict(self):
       for o_row in self.to_file_content:
         combined_row = o_row
-        combined_row['Annotation'] = ""
+        combined_row[self.field_to_add_name] = ""
 
         for i_row in self.from_file_content:
-          if i_row['Query'] == o_row['Name']:
-            combined_row['Annotation'] = i_row['Annotation']
+          if i_row[self.common_key_from] == o_row[self.common_key_to]:
+            combined_row[self.field_to_add_name] = i_row[self.field_to_add_name]
         self.result_content.append(combined_row)
+
+
+    # def make_res_dict(self):
+    #   for o_row in self.to_file_content:
+    #     combined_row = o_row
+    #     combined_row['Annotation'] = ""
+    #
+    #     for i_row in self.from_file_content:
+    #       if i_row['Query'] == o_row['Name']:
+    #         combined_row['Annotation'] = i_row['Annotation']
+    #     self.result_content.append(combined_row)
 
     def write_to_res_file(self):
       self.to_headers.append('Annotation')
@@ -74,7 +88,6 @@ if __name__ == '__main__':
                 required = False, action = "store", dest = "res_file_name", default = 'result_file.csv',
                 help = "Result CSV file name")
 
-
     parser.add_argument("-d", "--delimiter",
                 required = False, action = "store", dest = "delimiter", default = ',',
                 help = "CSV delimeter: comma, tab, space etc")
@@ -82,6 +95,20 @@ if __name__ == '__main__':
     parser.add_argument("-q", "--quotechar",
                 required = False, action = "store", dest = "quotechar", default = '"',
                 help = "CSV quote character: single or double quote")
+
+    parser.add_argument("-k_from", "--common_key_from",
+                required = False, action = "store", dest = "common_key_from", default = 'Query',
+                help = "Common key field name in FROM_file")
+
+    parser.add_argument("-k_to", "--common_key_to",
+                required = False, action = "store", dest = "common_key_to", default = 'Name',
+                help = "Common key field name in TO_file")
+
+    parser.add_argument("-f_add", "--field_to_add_name",
+                required = False, action = "store", dest = "field_to_add_name", default = 'Annotation',
+                help = "Field name from the FROM_file to be added to the result")
+
+
 
 # TODO: add key names args
 
