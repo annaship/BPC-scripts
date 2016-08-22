6829beab4a2a86680873466d2c3c1a36e5e64d77
annaship <tetya-loshad@yandex.ru>
2016-08-22 19:04:29 -0400

    write to file

--- py_scripts_mix/compare_silva.py
+++ py_scripts_mix/compare_silva.py
@@ -2,9 +2,8 @@ from collections import namedtuple
 
 class MySilva():
     def __init__(self):
-        # self.content_line = namedtuple('content_line', 'id start end tax') longer!
-        self.text = ""
-        
+        pass
+
     def get_file_content(self, in_filename):
         with open(in_filename, 'rb') as f:
             return f.readlines()
@@ -25,7 +24,7 @@ class MySilva():
             # print "id = %s; start = %s; end = %s; tax = %s" % (id, start, end, tax)
             parsed_content.append((id, start, end, tax))
         return parsed_content
-        
+
     def get_diff_tax_only(self, cont_n119, cont_n123):
         for t1 in cont_n123:
             for t2 in cont_n119:
@@ -37,7 +36,7 @@ class MySilva():
             for t2 in tax_content_119_p:
                 if ((t1[0] == t2[0]) and (t1[2] == t2[2]) and (t1[3] == t2[3]) and (t1[1] != t2[1])):
                     print "119 = %s\n123 = %s" % (" ".join(t2), " ".join(t1))
-        
+
     def get_diff_stop_only(self, cont_n119, cont_n123):
         for t1 in minus_both_content_123_p:
             for t2 in tax_content_119_p:
@@ -51,11 +50,12 @@ class MySilva():
                     print "119 = %s\n123 = %s" % (" ".join(t2), " ".join(t1))
 
     def get_same_tax_same_id_diff_start_or_diff_stop(self, cont_n119, cont_n123):
-        self.text = ""
+        text = ""
         for t1 in minus_both_content_123_p:
             for t2 in tax_content_119_p:
                 if ((t1[0] == t2[0]) and (t1[3] == t2[3]) and ((t1[1] != t2[1]) or (t1[2] != t2[2]))):
-                    self.text += "119 = %s\n123 = %s" % (" ".join(t2), " ".join(t1))
+                    text += "119 = %s\n123 = %s" % (" ".join(t2), " ".join(t1))
+        return text
 
 
 if __name__ == '__main__':
@@ -63,21 +63,22 @@ if __name__ == '__main__':
     tax_content_119 = util.get_file_content("refssu_headers_119.txt")
     tax_content_123 = util.get_file_content("refssu_headers_123_1.txt")
     minus_both_content_123 = util.get_file_content("headers123_minus_both.txt")
-    
+
     minus_both_content_123_p = util.parse_tax_content(minus_both_content_123)
     tax_content_119_p = util.parse_tax_content(tax_content_119)
 
     # util.get_diff_tax_only(tax_content_119_p, minus_both_content_123_p)
     # util.get_diff_stop_only(tax_content_119_p, minus_both_content_123_p)
-    util.get_same_tax_same_id_diff_start_or_diff_stop(tax_content_119_p, minus_both_content_123_p)
-    
+    text = util.get_same_tax_same_id_diff_start_or_diff_stop(tax_content_119_p, minus_both_content_123_p)
+    util.write_to_file("same_tax_same_id_diff_start_or_diff_stop", text):
+
     # for t1 in minus_both_content_123_p:
     #     for t2 in tax_content_119_p:
     #         if ((t1[0:2] == t2[0:2]) and (t1[3] != t2[3])):
     #             print "119 = %s\n123 = %s" % (t2, t1)
-    # 
+    #
     # print "+" * 10
     # print minus_both_content_123_p
-    # 
+    #
     # print "+" * 10
     # print tax_content_119_p
