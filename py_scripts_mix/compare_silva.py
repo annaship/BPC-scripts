03369004329c8c4e35ebb76a38dffb9c12ff6fa3
annaship <tetya-loshad@yandex.ru>
2016-08-22 19:02:49 -0400

    add other compares, write to file

--- py_scripts_mix/compare_silva.py
+++ py_scripts_mix/compare_silva.py
@@ -3,11 +3,16 @@ from collections import namedtuple
 class MySilva():
     def __init__(self):
         # self.content_line = namedtuple('content_line', 'id start end tax') longer!
-        pass
+        self.text = ""
         
     def get_file_content(self, in_filename):
         with open(in_filename, 'rb') as f:
             return f.readlines()
+
+    def write_to_file(self, file_name, text):
+        f = open(file_name, 'w')
+        f.write(text)
+        f.close
 
     def parse_tax_content(self, content_name):
         parsed_content = []
@@ -25,15 +30,33 @@ class MySilva():
         for t1 in cont_n123:
             for t2 in cont_n119:
                 if ((t1[0:2] == t2[0:2]) and (t1[3] != t2[3])):
-                    print "119 = %s\n123 = %s" % (t2, t1)
+                    print "119 = %s\n123 = %s" % (" ".join(t2), " ".join(t1))
 
-    def get_diff_stop_only(self, cont_n119, cont_n123):
+    def get_diff_start_only(self, cont_n119, cont_n123):
         for t1 in minus_both_content_123_p:
             for t2 in tax_content_119_p:
                 if ((t1[0] == t2[0]) and (t1[2] == t2[2]) and (t1[3] == t2[3]) and (t1[1] != t2[1])):
                     print "119 = %s\n123 = %s" % (" ".join(t2), " ".join(t1))
-
         
+    def get_diff_stop_only(self, cont_n119, cont_n123):
+        for t1 in minus_both_content_123_p:
+            for t2 in tax_content_119_p:
+                if ((t1[0] == t2[0]) and (t1[1] == t2[1]) and (t1[3] == t2[3]) and (t1[2] != t2[2])):
+                    print "119 = %s\n123 = %s" % (" ".join(t2), " ".join(t1))
+
+    def get_diff_stop(self, cont_n119, cont_n123):
+        for t1 in minus_both_content_123_p:
+            for t2 in tax_content_119_p:
+                if ((t1[0] == t2[0]) and (t1[3] == t2[3]) and (t1[2] != t2[2])):
+                    print "119 = %s\n123 = %s" % (" ".join(t2), " ".join(t1))
+
+    def get_same_tax_same_id_diff_start_or_diff_stop(self, cont_n119, cont_n123):
+        self.text = ""
+        for t1 in minus_both_content_123_p:
+            for t2 in tax_content_119_p:
+                if ((t1[0] == t2[0]) and (t1[3] == t2[3]) and ((t1[1] != t2[1]) or (t1[2] != t2[2]))):
+                    self.text += "119 = %s\n123 = %s" % (" ".join(t2), " ".join(t1))
+
 
 if __name__ == '__main__':
     util = MySilva()
@@ -45,8 +68,8 @@ if __name__ == '__main__':
     tax_content_119_p = util.parse_tax_content(tax_content_119)
 
     # util.get_diff_tax_only(tax_content_119_p, minus_both_content_123_p)
-    util.get_diff_stop_only(tax_content_119_p, minus_both_content_123_p)
-    
+    # util.get_diff_stop_only(tax_content_119_p, minus_both_content_123_p)
+    util.get_same_tax_same_id_diff_start_or_diff_stop(tax_content_119_p, minus_both_content_123_p)
     
     # for t1 in minus_both_content_123_p:
     #     for t2 in tax_content_119_p:
