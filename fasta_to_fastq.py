e4f191e3a88f87bf685f35d246de3de07be37354
annaship <tetya-loshad@yandex.ru>
2017-04-26 18:04:56 -0400

    make dicts

--- fasta_to_fastq.py
+++ fasta_to_fastq.py
@@ -15,44 +15,61 @@ qual_path = sys.argv[2]
 fq_path = sys.argv[3]
 
 
-# def init_unique_hash(self):
+# def init_unique_id(self):
 #     while self.next_regular():
-#         hash = hashlib.sha1(self.seq.upper()).hexdigest()
-#         if hash in self.unique_hash_dict:
-#             self.unique_hash_dict[hash]['ids'].append(self.id)
-#             self.unique_hash_dict[hash]['count'] += 1
+#         id = idlib.sha1(self.seq.upper()).hexdigest()
+#         if id in self.unique_id_dict:
+#             self.unique_id_dict[id]['ids'].append(self.id)
+#             self.unique_id_dict[id]['count'] += 1
 #         else:
-#             self.unique_hash_dict[hash] = {'id' : self.id,
+#             self.unique_id_dict[id] = {'id' : self.id,
 #                                            'ids': [self.id],
 #                                            'seq': self.seq,
 #                                            'count': 1}
 #
-#     self.unique_hash_list = [i[1] for i in sorted([(self.unique_hash_dict[hash]['count'], hash)\
-#                     for hash in self.unique_hash_dict], reverse = True)]
+#     self.unique_id_list = [i[1] for i in sorted([(self.unique_id_dict[id]['count'], id)\
+#                     for id in self.unique_id_dict], reverse = True)]
 #
 #
-#     self.total_unique = len(self.unique_hash_dict)
+#     self.total_unique = len(self.unique_id_dict)
 #     self.reset()
+def make_a_dict(f_path):
+  id_dict = {}
+  while f_path.next_regular():
+    id = f_path.id
+    id_dict[id] = f_path.seq
+  f_path.reset()
+  return id_dict
 
 f_input = fa.SequenceSource(fa_path)
 f_qual = fa.SequenceSource(qual_path)
-f_input.init_unique_hash()
-# while f_input.next():
-#   fa.init_unique_hash()
 
-f_input_dict = f_input.unique_hash_dict.items()
-f_qual_dict = f_qual.unique_hash_dict.items()
+f_input_dict = make_a_dict(f_input)
+f_qual_dict = make_a_dict(f_qual)
+# f_input.init_unique_id()
+# f_qual.init_unique_id()
+# while f_input.next():
+#   fa.init_unique_id()
+
+# fa.SequenceSource
+#
+# f_input_dict = f_input.unique_id_dict.items()
+# f_qual_dict = f_qual.unique_id_dict.items()
 # print f_input.total_unique
 # print f_qual.total_unique
+
 
 print "f_input_dict"
 print f_input_dict
 print "f_qual_dict"
 print f_qual_dict
 
+
 #   print
   # output.write(input.id + "#" + input.seq + "\n")
-  
+
+
+
 
 # make fastq
 """with open(fa_path, "r") as fasta, open(qual_path, "r") as qual, open(fq_path, "w") as fastq:
