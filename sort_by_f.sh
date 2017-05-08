71ebcec11fd4c955116db697b090eee09ecb675c
annaship <tetya-loshad@yandex.ru>
2017-05-08 15:09:49 -0400

    res file name. doesn't have not matching fields

--- sort_by_f.sh
+++ sort_by_f.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 FILE_TO_SORT="$1"
 INDEX_FILE="$2"
-TMP_FILE=$(mktemp)
+TMP_FILE="$1.sorted"
 
 while read -r LINE; do
     grep "$LINE" "$FILE_TO_SORT" >>"$TMP_FILE"
 done <"$INDEX_FILE"
 
-mv -f "$TMP_FILE" tmp.$$
+# mv -f "$TMP_FILE" tmp.$$
 
 # while read -r LINE; do echo $LINE; grep $LINE $FILE_TO_SORT >>$TMP_FILE; done <$INDEX_FILE ; cat what_to_sort.sorted.txt
