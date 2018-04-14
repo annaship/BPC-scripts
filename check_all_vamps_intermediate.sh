5e7b6707451294138d3bae59f9c920c3b1ad56ce
annaship <tetya-loshad@yandex.ru>
2018-04-14 18:32:08 -0400

    check_all_vamps_intermediate from server

--- check_all_vamps_intermediate.sh
+++ check_all_vamps_intermediate.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Check if no old data get lost. See check_all_vamps_intermediate.log. If output for any table is greater then 0 - investigate (if renamed, check in the opposite direction and compare resultes.) 
-# E.g. SELECT * FROM new_contact_intermediate a LEFT JOIN new_contact b USING(contact) WHERE b.contact_id IS NULL;
-# This script does not check vamps sequences - too slow.
-# ASh Oct 3 2015
 module load mysql/5.6.12; 
 rm check_all_vamps_intermediate.log
 
@@ -27,7 +23,7 @@ time mysql -nvv -h vampsdb vamps -e 'SELECT * FROM new_orderx_intermediate a RIG
 echo "SELECT * FROM new_phylum_intermediate a RIGHT JOIN new_phylum b USING(phylum) WHERE a.phylum_id IS NULL;"
 time mysql -nvv -h vampsdb vamps -e 'SELECT * FROM new_phylum_intermediate a RIGHT JOIN new_phylum b USING(phylum) WHERE a.phylum_id IS NULL;' >> check_all_vamps_intermediate.log 2>&1
 
-echo "SELECT count(*) FROM new_project_dataset_intermediate a RIGHT JOIN new_project_dataset b USING(project_dataset) WHERE a.project_dataset_id IS NULL;"
+echo "SELECT * FROM new_project_dataset_intermediate a RIGHT JOIN new_project_dataset b USING(project_dataset) WHERE a.project_dataset_id IS NULL;"
 time mysql -nvv -h vampsdb vamps -e 'SELECT * FROM new_project_dataset_intermediate a RIGHT JOIN new_project_dataset b USING(project_dataset) WHERE a.project_dataset_id IS NULL;' >> check_all_vamps_intermediate.log 2>&1
 
 echo "SELECT * FROM new_project_intermediate a RIGHT JOIN new_project b USING(project) WHERE a.project_id IS NULL;"
@@ -48,11 +44,11 @@ time mysql -nvv -h vampsdb vamps -e 'SELECT * FROM new_superkingdom_intermediate
 echo "SELECT * FROM new_taxon_string_intermediate a RIGHT JOIN new_taxon_string b USING(taxon_string) WHERE a.taxon_string_id IS NULL;"
 time mysql -nvv -h vampsdb vamps -e 'SELECT * FROM new_taxon_string_intermediate a RIGHT JOIN new_taxon_string b USING(taxon_string) WHERE a.taxon_string_id IS NULL;' >> check_all_vamps_intermediate.log 2>&1
 
-echo "SELECT count(*) FROM new_taxonomy_intermediate a RIGHT JOIN new_taxonomy b USING(taxon_string_id,superkingdom_id,phylum_id,class_id,orderx_id,family_id,genus_id,species_id,strain_id,rank_id,classifier) WHERE a.taxonomy_id IS NULL;"
-time mysql -nvv -h vampsdb vamps -e 'SELECT count(*) FROM new_taxonomy_intermediate a RIGHT JOIN new_taxonomy b USING(taxon_string_id,superkingdom_id,phylum_id,class_id,orderx_id,family_id,genus_id,species_id,strain_id,rank_id,classifier) WHERE a.taxonomy_id IS NULL;' >> check_all_vamps_intermediate.log 2>&1
+echo "SELECT * FROM new_taxonomy_intermediate a RIGHT JOIN new_taxonomy b USING(taxon_string_id,superkingdom_id,phylum_id,class_id,orderx_id,family_id,genus_id,species_id,strain_id,rank_id,classifier) WHERE a.taxonomy_id IS NULL;"
+time mysql -nvv -h vampsdb vamps -e 'SELECT * FROM new_taxonomy_intermediate a RIGHT JOIN new_taxonomy b USING(taxon_string_id,superkingdom_id,phylum_id,class_id,orderx_id,family_id,genus_id,species_id,strain_id,rank_id,classifier) WHERE a.taxonomy_id IS NULL;' >> check_all_vamps_intermediate.log 2>&1
 
-echo "SELECT count(*) FROM new_user_contact_intermediate a RIGHT JOIN new_user_contact b USING(contact_id,user_id) WHERE a.user_contact_id IS NULL;"
-time mysql -nvv -h vampsdb vamps -e 'SELECT count(*) FROM new_user_contact_intermediate a RIGHT JOIN new_user_contact b USING(contact_id,user_id) WHERE a.user_contact_id IS NULL;' >> check_all_vamps_intermediate.log 2>&1
+echo "SELECT * FROM new_user_contact_intermediate a RIGHT JOIN new_user_contact b USING(contact_id,user_id) WHERE a.user_contact_id IS NULL;"
+time mysql -nvv -h vampsdb vamps -e 'SELECT * FROM new_user_contact_intermediate a RIGHT JOIN new_user_contact b USING(contact_id,user_id) WHERE a.user_contact_id IS NULL;' >> check_all_vamps_intermediate.log 2>&1
 
 echo "SELECT * FROM new_user_intermediate a RIGHT JOIN new_user b USING(user) WHERE a.user_id IS NULL;"
 time mysql -nvv -h vampsdb vamps -e 'SELECT * FROM new_user_intermediate a RIGHT JOIN new_user b USING(user) WHERE a.user_id IS NULL;' >> check_all_vamps_intermediate.log 2>&1
@@ -63,7 +59,7 @@ time mysql -nvv -h vampsdb vamps -e 'SELECT count(*) FROM vamps_data_cube_interm
 echo "SELECT count(*) FROM vamps_junk_data_cube_intermediate a RIGHT JOIN vamps_junk_data_cube b USING(project_dataset,taxon_string) WHERE a.id IS NULL;"
 time mysql -nvv -h vampsdb vamps -e 'SELECT count(*) FROM vamps_junk_data_cube_intermediate a RIGHT JOIN vamps_junk_data_cube b USING(project_dataset,taxon_string) WHERE a.id IS NULL;' >> check_all_vamps_intermediate.log 2>&1
 
-echo "SELECT count(*) FROM vamps_projects_datasets_intermediate a RIGHT JOIN vamps_projects_datasets b USING(project,dataset) WHERE a.id IS NULL;"
+echo "SELECT * FROM vamps_projects_datasets_intermediate a RIGHT JOIN vamps_projects_datasets b USING(project,dataset) WHERE a.id IS NULL;"
 time mysql -nvv -h vampsdb vamps -e 'SELECT * FROM vamps_projects_datasets_intermediate a RIGHT JOIN vamps_projects_datasets b USING(project,dataset) WHERE a.id IS NULL;' >> check_all_vamps_intermediate.log 2>&1
 
 echo "SELECT * FROM vamps_projects_info_intermediate a RIGHT JOIN vamps_projects_info b USING(project_name,contact,email,institution) WHERE a.id IS NULL;"
