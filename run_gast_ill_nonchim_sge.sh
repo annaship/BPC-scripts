6a1b99b73eeece3b5d1060b7435a665d8f494df9
annaship <tetya-loshad@yandex.ru>
2019-08-17 13:11:11 -0400

    module load vsearch

--- run_gast_ill_nonchim_sge.sh
+++ run_gast_ill_nonchim_sge.sh
@@ -119,6 +119,7 @@ cat >clust_gast_ill_$RUN_LANE.sh <<InputComesFromHERE
 
   . /xraid/bioware/Modules/etc/profile.modules
   module load bioware
+  module load vsearch
 
   LISTFILE=./filenames.list
   INFILE=\`sed -n "\${SGE_TASK_ID}p" \$LISTFILE\`
