dde018f109b6711e116b50ceae21839930e08921
annaship <tetya-loshad@yandex.ru>
2019-09-16 12:02:21 -0400

    Update percent10_gast_unknowns.sh
    Works better with colors

--- percent10_gast_unknowns.sh
+++ percent10_gast_unknowns.sh
@@ -12,10 +12,14 @@ echo "================"
 lines=`wc -l $file`
 unknowns=`grep Unknown $file | wc -l`
 line_number=`echo $lines | awk '{print $1}'`
-perc=$(echo "scale = 2; $unknowns * 100 / $line_number" | bc -l)
 echo "Lines in file:    $lines"
 echo "Unknowns in file: $unknowns"
-#echo $perc | awk '{if ($perc > 10) print "\033[1;34mPercent unknowns:" $perc"\033[0m"}'
-echo $perc | awk '{if ($perc > 1) print "\033[1;34mPercent unknowns:" $perc"\033[0m"}'
+# echo $perc | awk '{if ($perc > 10) print "\033[1;34mPercent unknowns:" $perc"\033[0m"}'
+# to remove decimals:
+perc=$( printf '%.0f' $(echo "$unknowns * 100 / $line_number" | bc -l) )
+# echo "perc = $perc"
+if [ $perc -gt 1 ]; then
+  printf "\033[1;34mPercent unknowns: $perc \033[0m\n"
+fi
 
 done
